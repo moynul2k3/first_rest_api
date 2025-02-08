cd8c2bfe158728f9520566beb56ea76eb98a388d
moynul2k3 <moynul.officials@gmail.com>
2025-02-08 14:36:10 +0600

    hola

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jj188c8q)@q=9e)in72sv(b^e05t(ptn%_(nd!drgruw&@5w*$'
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = [ ]
 ALLOWED_HOSTS = ['127.0.0.1:8000', '.vercel.app', '*']
