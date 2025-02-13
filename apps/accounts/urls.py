4b625d8f889d50673847bcb26a0174f53ca44b2c
moynul2k3 <moynul.officials@gmail.com>
2025-02-14 04:34:01 +0600

    link update

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -4,12 +4,14 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
+    TokenBlacklistView,
 )
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('token/logout/', TokenBlacklistView.as_view(), name='logout'),
 
     path('check-user-exists/', check_user_exists, name='check_user_exists'),
     path('signin/', signin_view, name='signin'),
--- config/settings.py
+++ config/settings.py
@@ -7,9 +7,9 @@ SECRET_KEY = 'django-insecure-jj188c8q)@q=9e)in72sv(b^e05t(ptn%_(nd!drgruw&@5w*$
 DEBUG = True
 
 if DEBUG:
-    ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = ['www.localhost:8000']
 else:
-    ALLOWED_HOSTS = ["*"]
+    ALLOWED_HOSTS = ['.vercel.app', 'localhost:8000']
 
 
 INSTALLED_APPS = [
